339eef7e13dc83cafffbf653b8f128f5db8a523f
Bobby Lovell <rblovell@fairfielddd.com>
2021-12-07 08:30:05 -0500

    Sonar sweep.

--- src/Day 1 - Sonar Sweep/Sonar Sweep/Program.cs
+++ src/Day 1 - Sonar Sweep/Sonar Sweep/Program.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
 
 namespace Sonar_Sweep
 {
@@ -6,7 +11,35 @@ namespace Sonar_Sweep
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var inputPath = $@"{Environment.CurrentDirectory}\input.txt";
+
+            var inputText = File.ReadAllLines(inputPath).ToList();
+            var inputValues = new List<int>();
+            inputText.ForEach(line => inputValues.Add(int.Parse(line)));
+
+            int increases = 0, decreases = 0, nochange = 0;
+
+            for(int i = 0; i < inputValues.Count; i++)
+            {
+                if (i + 4 > inputValues.Count)
+                    break;
+
+                int sum = inputValues.GetRange(i, 3).Sum();
+                int sumNext = inputValues.GetRange(i + 1, 3).Sum();
+
+                Debug.WriteLine($"[{i}] Cur: {sum}  Nxt: {sumNext}");
+
+                if (sumNext > sum)
+                    increases++;
+                else if (sumNext < sum)
+                    decreases++;
+                else
+                    nochange++;
+            }
+
+            Debug.WriteLine($"Number of increases: {increases}");
+            Console.WriteLine($"Number of increases: {increases}");
+            Console.ReadKey();
         }
     }
 }
